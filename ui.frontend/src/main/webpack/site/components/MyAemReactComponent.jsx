1c670455b7cc4943159a02361dd16179855b1921
Jay Proulx <jproulx@tmg.io>
2021-02-19 09:30:23 -0500

    Added todo's

--- ui.frontend/src/main/webpack/site/components/MyAemReactComponent.jsx
+++ ui.frontend/src/main/webpack/site/components/MyAemReactComponent.jsx
@@ -13,9 +13,17 @@ class MyAemReactComponent extends React.Component {
     }
 }
 
-Array.prototype.slice.call(document.querySelectorAll('.my_aem_react_component')).map(function(element) {
-    ReactDOM.render(<MyAemReactComponent
-    title={ element.dataset.title }
-    description={ element.dataset.description }
-        />, element);
+// todo: this should be run on content loaded, rather than right away at the end of the page
+// todo: this worked in initial testing after editing the component, but needs further testing to ensure that if AEM updates the DOM, that the component
+// todo: this initialization code should be run such that after the element is initialized, it will not be initialized again if this runs a second time
+// todo: there's an opportunity to use the Sling Model Exporter to fetch data at load time
+
+Array.prototype.slice.call(document.querySelectorAll('.my_aem_react_component')).map(function (element) {
+    ReactDOM.render(
+        <MyAemReactComponent
+            title={element.dataset.title}
+            description={element.dataset.description}
+        />
+        , element
+    );
 });
